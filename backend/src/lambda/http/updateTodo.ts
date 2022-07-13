bef4b3c5871dbbdd00c3fb64c9c0fcc5c0c42461
Ancentus <mancentus7@gmail.com>
2022-07-14 01:39:25 +0300

    Implement update handler

--- backend/src/lambda/http/updateTodo.ts
+++ backend/src/lambda/http/updateTodo.ts
@@ -4,7 +4,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
 import * as middy from 'middy'
 import { cors, httpErrorHandler } from 'middy/middlewares'
 
-import { updateTodo } from '../../businessLogic/todos'
+import { updateTodo } from '../../helpers/todos'
 import { UpdateTodoRequest } from '../../requests/UpdateTodoRequest'
 import { getUserId } from '../utils'
 
@@ -12,10 +12,17 @@ export const handler = middy(
   async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     const todoId = event.pathParameters.todoId
     const updatedTodo: UpdateTodoRequest = JSON.parse(event.body)
-    // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
 
+    // DONE: Update a TODO item with the provided id using values in the "updatedTodo" object
+    console.log('Caller event', event)
+    const userId = getUserId(event)
+    await updateTodo(todoId, updatedTodo, userId);
 
-    return undefined
+    return {
+      statusCode: 200,
+      body: JSON.stringify(true),
+    }
+  }
 )
 
 handler
