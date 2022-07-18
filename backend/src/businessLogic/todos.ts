4327eaf65866eb220dc7d83ad2a8bee034823488
Ancentus <mancentus7@gmail.com>
2022-07-18 13:14:51 +0300

    Separate business logic and data layer

--- backend/src/businessLogic/todos.ts
+++ backend/src/businessLogic/todos.ts
@@ -1,5 +1,5 @@
-import { TodosAccess } from './todosAcess'
-import { AttachmentUtils } from './attachmentUtils';
+import { TodosAccess } from '../dataLayer/todosAcess'
+import { AttachmentUtils } from '../dataLayer/attachmentUtils';
 import { TodoItem } from '../models/TodoItem'
 import { CreateTodoRequest } from '../requests/CreateTodoRequest'
 //import { UpdateTodoRequest } from '../requests/UpdateTodoRequest'
